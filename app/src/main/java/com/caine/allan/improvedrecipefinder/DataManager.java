a0186b1c8dd8f74cd63d1b077cdab9244f1e2743
Allan Caine <adcaine@gmail.com>
2015-11-03 21:07:40 -0500

    Minor refactoring of DataManager

--- app/src/main/java/com/caine/allan/improvedrecipefinder/DataManager.java
+++ app/src/main/java/com/caine/allan/improvedrecipefinder/DataManager.java
@@ -1,12 +1,9 @@
 package com.caine.allan.improvedrecipefinder;
 
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.util.Log;
 
 import com.caine.allan.improvedrecipefinder.data.Recipe;
 import com.caine.allan.improvedrecipefinder.data.RecipeInterface;
-import com.caine.allan.improvedrecipefinder.data.RecipeSearchResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -14,11 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 
 import retrofit.converter.GsonConverter;
-import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -29,43 +24,41 @@ public class DataManager {
 
     private static final String TAG = "DataManager";
 
-    private static final String API_KEY = BuildConfig.API_KEY;
     private static final String FOOD2FORK_ENDPOINT = "http://food2fork.com/api";
     private List<Recipe> mRecipes;
     private List<RecipeSearchListener> mRecipeSearchListeners;
 
-    private RestAdapter mRestAdapter;
-    private RecipeInterface mRecipeInterface;
+    private static RestAdapter sRestAdapter;
+    private static RecipeInterface sRecipeInterface;
     private static DataManager sDataManager;
 
 
     private Context mContext;
 
-    protected DataManager(RestAdapter restAdapter, Context context){
+    protected DataManager(Context context){
         mContext = context;
-        mRestAdapter = restAdapter;
         mRecipeSearchListeners = new ArrayList<>();
         mRecipes = new ArrayList<>();
-        mRecipeInterface = mRestAdapter.create(RecipeInterface.class);
+
     }
 
     public static DataManager get(Context context){
         if(sDataManager == null){
             Gson gson = new GsonBuilder().create();
-            RestAdapter restAdapter = new RestAdapter.Builder()
+            sRestAdapter = new RestAdapter.Builder()
                     .setEndpoint(FOOD2FORK_ENDPOINT)
                     .setConverter(new GsonConverter(gson))
                     .setLogLevel(RestAdapter.LogLevel.FULL)
-                    .setRequestInterceptor(facade -> facade.addQueryParam("key", API_KEY))
+                    .setRequestInterceptor(facade -> facade.addQueryParam("key", BuildConfig.API_KEY))
                     .build();
-
-            sDataManager = new DataManager(restAdapter, context);
+            sRecipeInterface = sRestAdapter.create(RecipeInterface.class);
+            sDataManager = new DataManager(context);
         }
         return sDataManager;
     }
 
     public void fetchRecipes(String query){
-        mRecipeInterface.recipeSearch(query)
+        sRecipeInterface.recipeSearch(query)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnSubscribe(this::notifySearchListenersOnSearchStart)
